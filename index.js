b83d12fd2a5e7636a647e869dfb391f83d37547f
amiribnjaman <amiribnjaman@gmail.com>
2022-05-19 01:40:43 +0600

    Mongodb connected

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 50
+const port = process.env.PORT || 5000
+require('dotenv').config()
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mjkpv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+(async () => {
+    try {
+        await client.connect();
+        const taskCollection =  client.db("todoApp").collection("tasks");
+
+    }
+    finally {
+
+    }
+
+})().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`server running on port ${port}`)
 })
